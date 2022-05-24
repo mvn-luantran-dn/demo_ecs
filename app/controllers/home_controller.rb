22bab911eb26d8e965735fe12f404320ca16ad9a
Luan Tran N. T <luan.tran@monstar-lab.com>
2022-05-24 15:56:58 +0700

    Config Sidekiq

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,9 @@ class HomeController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def increment_async
+    ::IncrementCountWorker.perform_async(params[:post_id])
+    redirect_to root_path
+  end
 end
